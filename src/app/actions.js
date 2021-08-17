45fe19ba94fa0bd31ca12826f0aa104f6b4e8f9c
Dmytro Bedschuk <dmytro.beshchuk@protonmail.com>
2021-08-17 10:28:52 +0300

    Допил ДЗ

---
+++ src/app/actions.js
@@ -0,0 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
+export const setItems = createAction('SET_ITEMS')
+export const setFilter= createAction('SET_FILTER')
+export const deleteItem = createAction('DELETE_ITEM')
---
+++ src/app/reducer.js
@@ -0,0 +1,44 @@
+import { createReducer } from "@reduxjs/toolkit";
+import { setItems, setFilter, deleteItem } from "./actions";
+
+const defaultState = {
+  contacts: {
+    items: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  },
+};
+
+const contactsReducer = createReducer(defaultState, {
+  [setItems] (state, action) {
+    return {
+      ...state,
+      contacts: { items: [...action.payload], filter: state.contacts.filter },
+    };
+  },
+
+  [setFilter] (state, action) {
+    return {
+      ...state,
+      contacts: { items: [...state.contacts.items], filter: action.payload },
+    };
+  },
+
+  [deleteItem] (state, action) {
+    return {
+      ...state,
+      contacts: {
+        items: state.contacts.items.filter(
+          (item) => item.id !== action.payload
+        ),
+        filter: state.contacts.filter,
+      },
+    };
+  },
+});
+
+export default contactsReducer
